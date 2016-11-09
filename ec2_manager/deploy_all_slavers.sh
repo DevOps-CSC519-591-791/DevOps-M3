e13d41b2dc895c9140e0fedea55e6993faa97a4b
Jianfeng <ginfungchan@gmail.com>
2016-11-08 22:16:40 -0500

    fix bug

--- ec2_manager/deploy_all_slavers.sh
+++ ec2_manager/deploy_all_slavers.sh
@@ -3,12 +3,12 @@
 sudo node updateRedisIps.js
 echo 'smembers proxy'|redis-cli -h 54.234.163.154 > ips
 sudo rm slavers
-
+sudo echo "[aws_server]" >> "slavers"
 sudo cat ips | while read LINE
 do
-	sudo echo "aws_server ansible_ssh_host=$LINE ansible_ssh_user=ubuntu ansible_ssh_private_key_file=../keys/key4aws.pem" >> "slavers"
+	sudo echo "$LINE ansible_ssh_host=$LINE ansible_ssh_user=ubuntu ansible_ssh_private_key_file=../keys/key4aws.pem" >> "slavers"
 done
 
 sudo rm ips
 
-sudo ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i slavers deploy/deploy.yml
+sudo ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i slavers deploy/deploy.yml -vvv
