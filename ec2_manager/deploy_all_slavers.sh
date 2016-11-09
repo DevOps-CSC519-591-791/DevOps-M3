5b748e6e0f494e911799c1c4359ddc14b4cd7072
Jianfeng <ginfungchan@gmail.com>
2016-11-08 22:03:02 -0500

    --wip--

--- ec2_manager/deploy_all_slavers.sh
+++ ec2_manager/deploy_all_slavers.sh
@@ -2,13 +2,13 @@
 
 sudo node updateRedisIps.js
 echo 'smembers proxy'|redis-cli -h 54.234.163.154 > ips
-rm slavers
+sudo rm slavers
 
-cat ips | while read LINE
+sudo cat ips | while read LINE
 do
-	echo "aws_server ansible_ssh_host=$LINE ansible_ssh_user=ubuntu ansible_ssh_private_key_file=../keys/key4aws.pem" >> "slavers"
+	sudo echo "aws_server ansible_ssh_host=$LINE ansible_ssh_user=ubuntu ansible_ssh_private_key_file=../keys/key4aws.pem" >> "slavers"
 done
 
-rm ips
+sudo rm ips
 
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i slavers deploy/deploy.yml
+sudo ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i slavers deploy/deploy.yml
