a271da79a66f091727470378ea101d2dfc54e60d
drinkbeer <chenjb90@gmail.com>
2016-11-09 18:47:29 -0500

    update deploy

--- ec2_manager/ec2_creator.sh
+++ ec2_manager/ec2_creator.sh
@@ -5,4 +5,4 @@ echo 'start sleeping'
 sudo chmod 500 ../keys/key4aws.pem
 sleep 30s
 echo 'end sleeping'
-sudo ansible-playbook -i inventory deploy/deploy.yml -vvvv
+sudo ansible-playbook -i inventory deploy/deploy.yml
